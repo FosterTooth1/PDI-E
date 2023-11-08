841e6b10f92153295a16191ebbb39b0ad6a0a86b
FosterTooth1 <albertoflores91396@gmail.com>
2023-11-07 18:58:19 -0600

    Codigo de Huffman terminado

--- P09/HuffmanG.m
+++ P09/HuffmanG.m
@@ -10,12 +10,29 @@ probabilidades = histograma / total_pixeles;
 mensajes = 0:length(probabilidades)-1; % Ajustar el rango según la longitud de probabilidades
 dict = huffmandict(mensajes, probabilidades);
 
+% Mostrar los códigos Huffman
+for i = 1:length(dict)
+    fprintf('Símbolo %d: Codeword ', i-1);
+    disp(dict{i, 2});
+end
+
+% Mostrar los códigos Huffman en el fichero
+fichero = fopen('P09\Codes.txt', 'w');
+for i = 1:length(dict)
+    fprintf(fichero, 'Símbolo %d: Codeword %s\n', i-1, num2str(dict{i, 2}));
+end
+fclose(fichero);
+
 % Codificar la imagen
 a_vector = a(:); % Convertir la imagen en una matriz unidimensional
 a_codificado = huffmanenco(a_vector, dict);
 
 % Decodificar la imagen
 a_decodificado = huffmandeco(a_codificado, dict);
+
+% Comprobar si la el vector decodificado es igual a el vector de la imagen original
+isequal(a_vector, a_decodificado)
+
 a_decodificado = reshape(a_decodificado, size(a)); % Reconstruir la matriz
 
 % Comprobar si la imagen decodificada es igual a la original
@@ -23,20 +40,19 @@ isequal(a, a_decodificado)
 
 % Calcular la longitud promedio del código
 L = 0;
-for c = 1:length(probabilidades)
-    cantidadBits = length(dict{c, 2});
-    aux = cantidadBits * probabilidades(c);
+for i = 1:length(dict)
+    cantidadBits = length(dict{i, 2});
+    aux = cantidadBits * probabilidades(i);
     L = L + aux;
 end
-disp(['Longitud Promedio del Código (L): ' num2str(L)]);
+disp(['Longitud Promedio del Código Huffman (L): ' num2str(L)]);
 
 % Calcular la cantidad de información
 I = -log2(probabilidades);
 disp('Cantidad de Información (I):');
-for c = 1:length(I)
-    disp(['Valor ' num2str(c-1) ': ' num2str(I(c))]);
+for i = 1:length(I)
+    disp(['Valor ' num2str(i-1) ': ' num2str(I(i))]);
 end
-
 
 % Calcular la entropía
 H = sum(probabilidades .* I);
@@ -49,8 +65,3 @@ disp(['Rendimiento (Z): ' num2str(Z)]);
 % Calcular la redundancia
 R = 100 - Z;
 disp(['Redundancia (R): ' num2str(R)]);
-
-% Comprobar el teorema de Shannon
-if L >= H
-    disp('Se cumple el teorema de Shannon');
-end   
